2322d949fde3a4e0f552736132d19e673767dd54
Rachit Jain <rachitjain9596@gmail.com>
2024-03-01 22:56:16 +0530

    CreateOrder.jsx Order.jsx helpers.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from './ui/Home';
 import Menu from './features/menu/Menu';
 import Cart from './features/cart/Cart';
+import CreateOrder from './features/order/CreateOrder';
+import Order from './features/order/Order';
 
 const router = createBrowserRouter([
   {
@@ -15,6 +17,14 @@ const router = createBrowserRouter([
   {
     path: '/cart',
     element: <Cart />,
+  },
+  {
+    path: '/order/new',
+    element: <CreateOrder />,
+  },
+  {
+    path: '/order/:orderId',
+    element: <Order />,
   },
 ]);
 
