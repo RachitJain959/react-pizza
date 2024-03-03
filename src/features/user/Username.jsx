c2c4c31a707b7aabad5929ba0130e2d3c8033db7
Rachit Jain <rachitjain9596@gmail.com>
2024-03-03 23:07:28 +0530

    style text, border, spacing, display

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Ronny</div>;
+  return <div className="hidden text-sm font-semibold">Ronny</div>;
 }
 
 export default Username;
