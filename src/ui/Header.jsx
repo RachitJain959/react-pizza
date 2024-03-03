8937f5544dac00a2ef5c6a3d9356170d9752f589
Rachit Jain <rachitjain9596@gmail.com>
2024-03-03 18:16:24 +0530

    styling text

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="bg-yellow-500">
+    <header className="bg-yellow-500 uppercase tracking-widest">
       <Link to="/">Fast React Pizza Co.</Link>
       <SearchOrder />
-      <p>Ronny</p>
+      <Username />
     </header>
   );
 }
