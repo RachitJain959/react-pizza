7e1a8fe56288faaeb611e03cc91f8a970383d7e5
Rachit Jain <rachitjain9596@gmail.com>
2024-03-05 23:47:44 +0530

    style menu p1

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,16 +1,24 @@
-import { formatCurrency } from '../../utils/helpers';
+import { formatCurrency } from "../../utils/helpers";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
+    <li className="flex gap-4 py-2">
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
+      />
+      <div className="flex flex-col">
         <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+        <p>{ingredients.join(", ")}</p>
+        <div className="mt-auto text-sm">
+          {!soldOut ? (
+            <p>{formatCurrency(unitPrice)}</p>
+          ) : (
+            <p className="font-medium uppercase text-stone-500">Sold out</p>
+          )}
         </div>
       </div>
     </li>
