3cb1c49028898959f745fe4c1b6fc4d18885f424
Rachit Jain <rachitjain9596@gmail.com>
2024-03-09 15:31:59 +0530

    redux thunks extraReducers cases:pending, fulfilled, rejected

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -22,6 +22,7 @@ export const fetchAddress = createAsyncThunk(
     const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
 
     // 3) Then we return an object with the data that we are interested in
+    // PAYLOAD OF THE FULFILLED STATE
     return { position, address };
   },
 );
@@ -40,6 +41,20 @@ const userSlice = createSlice({
       state.username = action.payload;
     },
   },
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchAddress.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchAddress.fulfilled, (state, action) => {
+        state.position = action.payload.position;
+        state.address = action.payload.address;
+        state.status = "idle";
+      })
+      .addCase(fetchAddress.rejected, (state, action) => {
+        state.status = "error";
+        state.error = action.error.message;
+      }),
 });
 
 export const { updateName } = userSlice.actions;
